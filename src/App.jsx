62acc71defc24b97b78e9bb839674477439118ff
Keerthi-1206 <Keerthi.neeradi05@gmail.com>
2024-12-16 10:45:34 +0530

    changes the import file names

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router,Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home/Home";
-import Header from "./components/common/Header/Header";
-import Footer from "./components/common/Footer/Footer";
+import Header from "./components/Common/Header/Header";
+import Footer from "./components/Common/Footer/Footer";
 import About from "./pages/About/About";
-import Contact from "./pages/Contact/Contact";
+import Contact from "./pages/About/About";
 import Tours from "./pages/Tours/Tours";
 import TourDetails from "./pages/Tours/TourDetails";
 import Booking from "./pages/Booking/Booking";
